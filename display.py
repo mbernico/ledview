92b8b4fdc4d3a421d6e032b29db878ea7727c528
Mike Bernico <mike.bernico@gmail.com>
2024-07-28 13:25:58 -0700

    updated display for news

--- display.py
+++ display.py
@@ -3,27 +3,37 @@ import datetime
 import time
 import os
 import pytz
+import asyncio
 
 import rgbmatrix
 from rgbmatrix import core
 from rgbmatrix import graphics
+# https://python-weather.readthedocs.io/en/latest/
+import python_weather
+# https://pypi.org/project/pynytimes/
+from pynytimes import NYTAPI
 
 from colors import Colors
+import secrets
 
 # Facts about my display.
 _TIME_ZONE = "America/Los_Angeles" # My Local Time Zone
+_LOCATION = "Benicia, CA"
+_WEATHER_SLEEP = 3600 # Update weather hourly.
+_NEWS_SLEEP = 3600 # Update news every hour.
 _LED_COLS = 64
 _LED_ROWS = 64
 _LED_CHAIN = 3
 # NOTE: This file must be specified relative to the source code location due to how the bindings are written.
 # TODO: Maybe update the original code to fix this someday???
-_FONT_PATH = "fonts/7x13.bdf"
 _TIME_FONT_PATH = "fonts/9x15B.bdf"
 _DATE_FONT_PATH = "fonts/7x13.bdf"
+_WEATHER_FONT_PATH = "fonts/5x7.bdf"
+
 
 class Display:
     """Setups and drives the LED Matrix display."""
-    def __init__(self, cols, rows, chain, date_font_path, time_font_path, time_zone):
+    def __init__(self, cols, rows, chain):
         options = rgbmatrix.RGBMatrixOptions()
         options.rows = cols
         options.cols = rows
@@ -31,13 +41,33 @@ class Display:
         self.matrix = rgbmatrix.RGBMatrix(options = options)
         self.date_font = graphics.Font()
         self.time_font = graphics.Font()
+        self.weather_font = graphics.Font()
         try: 
-            self.date_font.LoadFont(date_font_path)
-            self.time_font.LoadFont(time_font_path)
+            self.date_font.LoadFont(_DATE_FONT_PATH)
+            self.time_font.LoadFont(_TIME_FONT_PATH)
+            self.weather_font.LoadFont(_WEATHER_FONT_PATH)
         except FileNotFoundError as e:
             e.strerror("The font files can't be opened.")
         
-        self.time_zone = pytz.timezone(time_zone)
+        self.time_zone = pytz.timezone(_TIME_ZONE)
+
+        self.offscreen_canvas = self.matrix.CreateFrameCanvas()
+        self.weather = None
+        self.weather_update_time = None
+        self.nyt = NYTAPI(secrets.NYT_API_KEY, parse_dates=True)
+        self.nyt_update_time = None
+        self.top_stories = None
+
+    
+    async def _maybe_update_weather(self) -> python_weather.forecast.Forecast:
+        """Update the weather from our API if it's old enough."""
+        if (
+            (self.weather_update_time == None) or 
+            ((self.weather_update_time - datetime.datetime.now()).total_seconds() > _WEATHER_SLEEP)):
+            async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+                self.weather = await client.get(_LOCATION)
+                self.weather_update_time = datetime.datetime.now()
+
 
     
     @staticmethod
@@ -58,55 +88,77 @@ class Display:
             time_color = graphics.Color(*Colors.RED.value)
         return time_color, time.strftime("%H:%M:%S:%Z")
             
-    def _draw_date(self, offscreen_canvas: core.FrameCanvas) -> None:
+    def _draw_date(self) -> None:
         """Draws the date"""
         date = self._get_date()
         date_color = graphics.Color(*Colors.GREEN.value)
-        graphics.DrawText(offscreen_canvas, self.date_font, 1, self.date_font.baseline * 2 + 2, date_color, date)
+        graphics.DrawText(self.offscreen_canvas, self.date_font, 10, self.date_font.baseline * 2 + 10, date_color, date)
 
-    def _draw_time(self, offscreen_canvas: core.FrameCanvas ) -> None:
+    def _draw_time(self) -> None:
         """Draws the time."""
         time_color, current_time = self._get_time_alternating()
-        graphics.DrawText(offscreen_canvas, self.time_font, 1, self.time_font.baseline, time_color, current_time)
+        graphics.DrawText(self.offscreen_canvas, self.time_font, 1, self.time_font.baseline, time_color, current_time)
+
+    def _draw_weather(self) -> None:
+        """Draws the weather"""
+        #weather = asyncio.run(self._get_weather())
+        weather_color = graphics.Color(*Colors.PURPLE.value)
+        weather_string = f"{self.weather.temperature}" + u'\N{DEGREE SIGN}' + " " + self.weather.wind_direction.value + " " + str(self.weather.wind_speed)
+        kind_string = f"{str(self.weather.kind)}"
+        graphics.DrawText(self.offscreen_canvas, self.weather_font, 120, 1 + self.weather_font.baseline, weather_color, weather_string)
+        graphics.DrawText(self.offscreen_canvas, self.weather_font, 120, 3 + self.weather_font.baseline *2, weather_color, kind_string)
+    
+    def _get_top_stories(self) -> None:
+        titles = []
+        if (
+            (self.nyt_update_time == None) or 
+            ((self.nyt_update_time - datetime.datetime.now()).total_seconds() > _NEWS_SLEEP)):
+            self.nyt_update_time = datetime.datetime.now()
+            top_stories = self.nyt.top_stories()
+
+            for story in top_stories:
+                titles.append(story['title'])
+        
+            self.top_stories = " ".join(titles)
+
+
 
 
     def run(self):
         """Driver loop for display."""
-        offscreen_canvas = self.matrix.CreateFrameCanvas()
-
-        date_color = graphics.Color(255,255,0)
-        time_color = date_color
-        pos = offscreen_canvas.width
+        scroll_position = self.offscreen_canvas.width
 
         while True:
-            offscreen_canvas.Clear()
+            self.offscreen_canvas.Clear()
+            asyncio.run(self._maybe_update_weather())
 
-            self._draw_time(offscreen_canvas)
-            self._draw_date(offscreen_canvas)
+            self._draw_time()
+            self._draw_date()
+            self._draw_weather()
+            self._get_top_stories()
+
+            news_scroll_len = graphics.DrawText(
+                self.offscreen_canvas,
+                self.date_font,
+                scroll_position,
+                self.date_font.baseline * 5 + 5,
+                graphics.Color(*Colors.PURPLE.value),
+                self.top_stories)
+            
+            scroll_position -= 1
+            if (scroll_position + news_scroll_len < 0):
+                scroll_position = self.offscreen_canvas.width
 
 
-            #length = graphics.DrawText(offscreen_canvas, self.font, pos, 0 + self.font.baseline * 2, date_color, current_time + date)
-            #pos -=1
-            #if (pos + length < 0):
-            #    pos = offscreen_canvas.width
+            
             time.sleep(0.05)
 
-
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-
-
-
-
-
-
-
-
+            self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
 
 def main():
     """Uh, main..."""
-    display = Display(_LED_COLS, _LED_ROWS, _LED_CHAIN, _DATE_FONT_PATH, _TIME_FONT_PATH, _TIME_ZONE)
+    display = Display(_LED_COLS, _LED_ROWS, _LED_CHAIN)
     display.run()
-
 
 if __name__ == "__main__":
     main()
